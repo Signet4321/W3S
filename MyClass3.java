25aebfe834a7a0d5367f84c7bacdd835273b076c
Signet4321 <addocorwin3@gmail.com>
2019-06-18 17:48:58 +0000

    for multiple objects

---
+++ MyClass3.java
@@ -0,0 +1,12 @@
+public class MyClass3{
+  int x = 5;
+
+  public static void main(String[] args) {
+    MyClass3 myObj1 = new MyClass3();  
+    MyClass3 myObj2 = new MyClass3();  
+myObj2.x=25;
+System.out.println(myObj1.x);
+System.out.println(myObj2.x); 
+  }
+}
+
