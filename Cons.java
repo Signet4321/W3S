458a5b004d562b547c0aac8ec4e5fa9d51b29019
Signet4321 <addocorwin3@gmail.com>
2019-06-18 20:41:14 +0000

    using java constructors

---
+++ Cons.java
@@ -0,0 +1,13 @@
+public class Cons {
+  int x; 
+
+ 
+  public Cons() {
+    x = 5;  
+  }
+
+  public static void main(String[] args) {
+    Cons  myObj = new Cons(); 
+    System.out.println(myObj.x);
+  }
+}
