d390ddfb15b08b4b7ab197d95cfa9a92d42b16a7
Signet4321 <addocorwin3@gmail.com>
2019-06-18 21:06:36 +0000

    Compare to Drift.java, Abstract under java modifiers

---
+++ Abs.java
@@ -0,0 +1,26 @@
+abstract class Abs {
+  public String fname = "John";
+  public int age = 24;
+  public abstract void study(); 
+}
+
+
+class Student extends Abs {
+  public int graduationYear = 2018;
+  public void study() { 
+    System.out.println("Studying all day long");
+  }
+}
+
+
+
+class Dif1 {
+  public static void main(String[] args) {
+ Student myObj = new Student(); 
+
+    System.out.println("Name: " + myObj.fname);
+    System.out.println("Age: " + myObj.age);
+    System.out.println("Graduation Year: " + myObj.graduationYear);
+    myObj.study(); 
+}
+}
