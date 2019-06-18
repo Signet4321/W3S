18c5c3a8838166a6472dbe0241d4842f314eba74
Signet4321 <addocorwin3@gmail.com>
2019-06-18 18:08:50 +0000

    Static or Public

---
+++ MyClass4.java
@@ -0,0 +1,19 @@
+public class MyClass4 {
+
+  static void myStaticMethod() {
+    System.out.println("Static methods can be called without creating objects");
+  }
+
+
+  public void myPublicMethod() {
+    System.out.println("Public methods must be called by creating objects");
+  }
+
+
+  public static void main(String[] args) {
+    myStaticMethod();
+
+    MyClass4 myObj = new MyClass4(); 
+  myObj.myPublicMethod();
+  }
+}
