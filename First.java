644b3980d7168ba747489c28c81205eaec57043b
Signet4321 <addocorwin3@gmail.com>
2019-06-18 16:32:44 +0000

    Creating Objects and multiple objects

---
+++ First.java
@@ -0,0 +1,9 @@
+public class First{
+int x=5;
+public static void main(String[] args) {
+First myObj1 = new First();
+First myObj2 = new First();
+System.out.println(myObj1.x);
+System.out.println(myObj2.x);
+}
+}
