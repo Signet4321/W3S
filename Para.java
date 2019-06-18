c008d19ced961027e2fb524a399d3f6bdd0f3183
Signet4321 <addocorwin3@gmail.com>
2019-06-18 20:44:30 +0000

    using constructor parameters

---
+++ Para.java
@@ -0,0 +1,13 @@
+public class Para{
+  int x;
+
+  public Para(int y) {
+    x = y;
+  }
+
+  public static void main(String[] args) {
+    Para myObj = new Para(5);
+    System.out.println(myObj.x);
+  }
+}
+
